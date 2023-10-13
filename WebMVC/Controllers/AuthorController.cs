428f6f8f1d39df77a43847afb4e3e4f7185dfa50
mohamed ahmed rabie <rabie3500@gmail.com>
2023-10-14 00:37:30 +0300

    Add projectMVC And using  HttpClient

---
+++ WebMVC/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json;
+using WebMVC.Models;
+
+namespace WebMVC.Controllers
+{
+    public class AuthorController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:44314/api");
+        private readonly HttpClient _client;
+        public AuthorController(HttpClient client)
+        { 
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+  
+        [HttpGet]
+        public async Task<IActionResult> GetAuthors()
+        {
+            var auth = new List<Author>();
+            var response = await _client.GetAsync(_client.BaseAddress +"/Authors/AllAuthors");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                auth = JsonConvert.DeserializeObject<List<Author>>(data); 
+            }
+            return View(auth);
+        }
+    }
+}
---
+++ WebMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebMVC.Models;
+
+namespace WebMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WebMVC.Models.Author> Author { get; set; } = default!;
+    }
+}
